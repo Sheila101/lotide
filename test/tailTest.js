5cf7a4c593ab4c223b54b0fd28fff862ed32b7a8
vagrant <sheila.kuria@ontariotechu.net>
2022-12-11 17:06:36 +0000

    used chai and mocha for testing, created an index.js file so that all the functions we need for lotide are there in an object - need to come back to test out the test case that failing in headTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,13 @@
+const assert = require('chai').assert;
+
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 //calling this to test
-tail(['milk', 'eggs', 'sugar']);
+//tail(['milk', 'eggs', 'sugar']);
+
+describe('#tail', () => {
+  it('returns "sugar" for ["milk", "eggs", "sugar"]', () => {
+    assert.deepEqual(tail(['milk', 'eggs', 'sugar']), ['eggs','sugar']);
+  });
+});
