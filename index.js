a961c50db4f73ba1a5f21bcf3e963d604cb566e0
vagrant <sheila.kuria@ontariotechu.net>
2023-01-22 18:14:23 +0000

    Edited the index.js file as well as added a README file

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const middle = require('./middle');
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKeys = require('./findKeys');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   head,
@@ -13,4 +24,15 @@ module.exports = {
   eqArrays,
   assertEqual,
   assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKeys,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
 };
